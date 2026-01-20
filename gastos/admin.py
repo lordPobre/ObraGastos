98535aabcce491f3defba78da3700db7a2db4aaa
CarlosLopez <131546244+lordPobre@users.noreply.github.com>
2026-01-20 14:16:34 -0300

    Actualizar admin.py

--- gastos/admin.py
+++ gastos/admin.py
@@ -4,14 +4,14 @@ from django.utils.html import format_html
 
 @admin.register(Gasto)
 class GastoAdmin(admin.ModelAdmin):
-    # Columnas que se verán en la tabla
+    
     list_display = ('id', 'usuario', 'mostrar_monto', 'fecha_emision', 'procesado_exitosamente', 'ver_imagen')
     list_filter = ('procesado_exitosamente', 'fecha_subida')
     search_fields = ('rut_emisor', 'folio')
     readonly_fields = ('fecha_subida',)
 
     def mostrar_monto(self, obj):
-        # Formato chileno de pesos
+        
         return f"${obj.monto_total:,.0f}".replace(",", ".")
     mostrar_monto.short_description = "Monto Total"
 
