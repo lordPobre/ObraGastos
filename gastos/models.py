198756993e23f59aaf5252904b2f015dc61e1d95
CarlosLopez <131546244+lordPobre@users.noreply.github.com>
2026-01-20 14:19:02 -0300

    Actualizar models.py

--- gastos/models.py
+++ gastos/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-# 1. Nuevo Modelo para las Obras
 class Obra(models.Model):
     nombre = models.CharField(max_length=100)
     direccion = models.CharField(max_length=200, blank=True, null=True)
@@ -12,17 +11,13 @@ class Obra(models.Model):
 
 class Gasto(models.Model):
     usuario = models.ForeignKey(User, on_delete=models.CASCADE)
-    # 2. Conectamos el Gasto a la Obra (null=True permite que existan gastos sin obra asignada por ahora)
     obra = models.ForeignKey(Obra, on_delete=models.SET_NULL, null=True, blank=True)
-    
     imagen = models.FileField(upload_to='boletas/', verbose_name="Boleta o Factura")
     fecha_subida = models.DateTimeField(auto_now_add=True)
-    
     rut_emisor = models.CharField(max_length=20, blank=True, null=True)
     folio = models.CharField(max_length=50, blank=True, null=True)
     fecha_emision = models.DateField(blank=True, null=True)
     monto_total = models.IntegerField(default=0)
-    
     procesado_exitosamente = models.BooleanField(default=False)
     nota_error = models.TextField(blank=True, null=True)
 
